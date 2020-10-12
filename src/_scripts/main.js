3980996a2d48f1b567ce5799a57ff6c5b51558a1
Mike <migueljrojas@gmail.com>
2020-10-12 10:30:46 -0300

    Initial commit

---
+++ src/_scripts/main.js
@@ -0,0 +1,16 @@
+// Main javascript entry point
+// Should handle bootstrapping/starting application
+
+'use strict';
+
+global.$ = global.jQuery;
+//global._ = require('underscore');
+
+var Header = require('../_modules/header/header');
+var Slider = require('../_modules/slider/slider');
+
+$(function() {
+
+    new Header();
+    new Slider();
+});
